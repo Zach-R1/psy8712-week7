002b8e1e78b58ab8805b99f27b8543ada99cb2ea
Zachariah R. Roper <roper063@umn.edu>
2026-03-15 20:48:31 -0500

    Data Import and Cleaning

--- R/week7.R
+++ R/week7.R
@@ -1,20 +1,20 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-#
-#
-#
-#
+library(tidyverse)
+# Additional Library
+# Additional Library
+# Additional Library
 
 # Data Import and Cleaning
-
-
-
-
-
-
-
-
-
+week7_tbl <- read_csv("../data/week3.csv", col_names = TRUE) %>%
+  mutate(condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control")),
+         gender = factor(gender, levels = c("M", "F"), labels = c("Male", "Female")),
+         timeStart = ymd_hms(timeStart),
+         timeEnd = ymd_hms(timeEnd),
+         across(q1:q10, as.integer)) %>%
+  filter(q6 == 1) %>%
+  select(-q6) %>%
+  mutate(timeSpent = timeEnd - timeStart)
 
 
 
