394d477ac4a5ea64354ca6630e683e8cb81dacfb
Zachariah R. Roper <roper063@umn.edu>
2026-03-15 21:46:17 -0500

    First two visualizations generated

--- R/week7.R
+++ R/week7.R
@@ -1,7 +1,7 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-# Additional Library
+library(GGally)
 # Additional Library
 # Additional Library
 
@@ -19,8 +19,14 @@ week7_tbl <- read_csv("../data/week3.csv", col_names = TRUE) %>%
 
 
 # Visualization
-
-
+week7_tbl %>%
+  ggpairs(columns = grep("^q", names(week7_tbl))) %>%
+  ggsave(filename = "../figs/multivisual.png", height = 9, width = 9, units = "in", dpi = 600)
+(week7_tbl %>%
+  ggplot(aes(timeStart, q1)) +
+  geom_point() +
+  labs(x = "Date of Experiment", y = "Q1 Score")) %>%
+  ggsave("../figs/figs1.png", plot = ., height = 3, width = 6, units = "in", dpi = 600)
 
 
 
